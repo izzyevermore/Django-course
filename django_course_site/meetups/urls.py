36388c54e2427726413317736221de1b50b7a693
Isaiah <isaiahlekay2001@gmail.com>
2021-06-24 15:56:04 +0200

    ran into some trouble with getting the server run

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups', views.starting_page) # our-domain.com/meetups
+    path('meetups/', views.index) # our-domain.com/meetups
 ]
